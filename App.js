de9f7414e0f7c0b9c7dbf0cf6dcffa9c27f38881
Pablo Jurado <pjurado85@gmail.com>
2023-04-03 10:02:59 -0500

    create simple todo app

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [items, setItems] = React.useState([]);
+  const [text, onChangeText] = React.useState("");
+
+  function AddItem() {
+    setItems([...items, text]);
+    onChangeText("");
+  }
+
+  function removeItem(removeIndex) {
+    setItems(items.filter((item, index) => index !== removeIndex));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
+
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder="new todo"
+        />
+        <Button title="Add" onPress={AddItem} disabled={text === ""} />
+
+        <FlatList
+          data={items}
+          keyExtractor={(item) => item}
+          renderItem={({ item, index }) => (
+            <TodoItem itemText={item} onPress={() => removeItem(index)} />
+          )}
+        />
+      </View>
+    </>
+  );
+}
+
+function TodoItem({ itemText, onPress }) {
+  return (
+    <View style={styles.listItem}>
+      <Text>{itemText}</Text>
+      <Button title="Remove" onPress={onPress}></Button>
     </View>
   );
 }
@@ -13,8 +59,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "flex-start",
+    margin: 50,
+  },
+  list: {
+    margin: 5,
+  },
+  listItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 5,
+    padding: 5,
+    borderStyle: "dashed",
+    borderWidth: 1,
+  },
+  input: {
+    height: 40,
+    margin: 5,
+    borderWidth: 1,
+    padding: 10,
   },
 });
